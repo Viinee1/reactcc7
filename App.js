eb7731c0bc171315c89ff296e0c88b36fc76a804
Vinicius Rayol <102189417+Viinee1@users.noreply.github.com>
2024-02-20 23:34:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar, Image, Pressable } from 'react-native';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [valor, setValor] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState('#fff');
+  const [cliquesfundo, setFundo] = useState(0);
+  const [imagem, setImagem] = useState('./images/jack.jpeg');
+
+
+  const trocarImagem = () => {
+    setImagem('./images/mp.jpg');
+  };
+
+  const subtrairContador = () => {
+    setValor(valor - 1);
+  };
+
+  const atualizarFundo = () => {
+    setFundo(cliquesfundo + 1);
+
+    if (cliquesfundo >= 0) {
+      setBackgroundColor('green');
+    }
+  };
+
+  const atualizarValor = () => {
+    setValor(valor + 1);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Pressable onPress={atualizarFundo} style={[styles.container, { backgroundColor }]}>
+    <View style={[styles.container, { backgroundColor }]}>
       <StatusBar style="auto" />
+      <Pressable onPress={trocarImagem}>
+        <Image source={imagem} 
+        style={styles.image} />
+      </Pressable>
+      <br></br>
+      <Pressable onPress={subtrairContador}>
+        <Text>{valor}</Text>
+      </Pressable>
+      <Pressable onPress={atualizarValor}>
+        <Text style={styles.button}>somar</Text>
+      </Pressable>
     </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "purple",
+    color: "white",
+  },
+  image: {
+    width: 300,
+    height: 450,
+    borderRadius: 20,
+    borderStyle: 'solid',
+    borderColor: 'yellow',
+    borderWidth: 'medium'
+  }
 });
